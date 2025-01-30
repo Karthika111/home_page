369409301c29be38166e2270f3c9781d7098748f
karthika <karthikasbabugmail.com>
2025-01-30 14:47:55 +0530

    30-01-2025

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import './Banner.css'; // For custom styles
+import banner2 from '../assets/banner2.jpg'; // Import the image directly
 
 const Banner = () => {
   return (
-    
     <div className="banner-slider">
       <div className="welcome-content">
         <h2>Welcome to Our Website!</h2>
         <p>We are thrilled to have you here. Explore our amazing services and feel free to get in touch with us for any questions or inquiries.</p>
       </div>
       <div>
-        <img src="/src/assets/banner2.jpg" alt="Banner 1" className="banner-image" />
+        <img src={banner2} alt="Banner 1" className="banner-image" />
       </div>
-
-    
-      
     </div>
   );
 };
